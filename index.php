b3fb6542ab224c1a50c20977a91b8ec191393042
QuentinDll <quentin.delplace13@gmail.com>
2018-07-31 14:30:21 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 1</title>
+</head>
+<body>
+  <p>
+    <?php
+    $num = 1;
+    while ($num < 10){
+      echo $num ++ . " ";
+    }
+    ?>
+  </p>
+</body>
+</html>
